628e5e0e6d9a01ceab80ca054af704897c77376e
Thibaud-Stanislas <stanislas.thibaud@gmail.com>
2023-04-03 11:27:12 +0200

    cours JS

--- cours/index.js
+++ cours/index.js
@@ -18,15 +18,17 @@ btn2.addEventListener("click", () => {
 
 const mousemove = document.querySelector(".mousemove");
 window.addEventListener("mousemove", (e) => {
+  console.log(e);
   mousemove.style.left = e.pageX + "px";
   mousemove.style.top = e.pageY + "px";
 });
 
 window.addEventListener("mousedown", () => {
   mousemove.style.transform = "scale(2) translate(-25%, -25%)";
+  mousemove.style.border = "2px solid teal";
 });
 
 window.addEventListener("mouseup", () => {
   mousemove.style.transform = "scale(1) translate(-50%, -50%)";
-  mousemove.style.border = "2px solid teal";
+  mousemove.style.border = "2px solid pink";
 });
